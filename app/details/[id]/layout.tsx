0978f734b8792cc8c9262c8c6270c90b57b1ed22
onody <onoderayuto@onody00.local>
2025-03-18 22:13:40 +0900

    メタデータ設定

---
+++ app/details/[id]/layout.tsx
@@ -0,0 +1,53 @@
+// app/details/[id]/layout.tsx
+
+import { Metadata } from "next";
+
+// メタデータ生成 (サーバーサイド)
+export async function generateMetadata({
+  params,
+}: {
+  params: { id: string };
+}): Promise<Metadata> {
+  const res = await fetch(`https://api.notion.com/v1/pages/${params.id}`, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${process.env.NOTION_API_KEY}`,
+      "Notion-Version": "2022-06-28",
+    },
+  });
+
+  if (!res.ok) {
+    throw new Error(`Notion API request failed: ${res.status}`);
+  }
+
+  const pageData = await res.json();
+  const pageTitle =
+    pageData?.properties?.Name?.title?.[0]?.text?.content || "moveeブログ";
+  const pageDescription =
+    pageData?.properties?.description?.rich_text?.[0]?.text?.content || "説明なし";
+
+  return {
+    title: pageTitle,  // 動的なタイトルを設定
+    description: pageDescription,  // description
+    openGraph: { // Open Graph メタデータ
+        title: pageTitle,
+        description: pageDescription,
+        images:[
+            {
+                url: "https://placehold.jp/1200x630.png", // Replace with the actual URL of the image
+                width: 1200,
+                height: 630,
+                alt: pageTitle,
+            },
+        ],
+    },
+  };
+}
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div>
+      {children}
+    </div>
+  );
+}
