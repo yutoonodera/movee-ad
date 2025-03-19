b919ce1520a253dc0c1ef59e51cab1276fd0ca52
onody <onoderayuto@onody00.local>
2025-03-19 16:17:54 +0900

    コメントアウト削除

--- app/components/OGPCard.tsx
+++ app/components/OGPCard.tsx
@@ -11,16 +11,6 @@ export default function OGPCard({ url }: Props) {
       <Card
         hoverable
         style={{ maxWidth: 300, marginBottom: "20px" }}
-        // cover={
-        //   <img
-        //     alt="OGP Image"
-        //     src={`https://api.opengraph.io/api/1.1/site/${encodeURIComponent(url)}?app_id=e3d9561c-ed10-4230-81ad-5a2539ea233c`}
-        //     onError={(e) => {
-        //       // Fallback image in case OGP image is not available
-        //       (e.target as HTMLImageElement).src = "/placeholder-image.png";
-        //     }}
-        //   />
-        // }
       >
         <Card.Meta title={url} />
       </Card>
