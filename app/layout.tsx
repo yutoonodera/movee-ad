adb8938a303b5412dcf103012b0c8e62abf7d7f5
onody <onoderayuto@onody00.local>
2025-03-22 12:56:40 +0900

    トップページのメタ情報設定

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,19 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'moveeブログ',
   description: '株式会社moveeの公式ブログです。ソフトウェア開発に関する情報を発信しています。',
+  openGraph: {
+    title: 'moveeブログ',
+    description: '株式会社moveeの公式ブログです。ソフトウェア開発に関する情報を発信しています。',
+    images: [
+      {
+        url: '/images/default.png', // public 内のOGP画像
+        width: 1200,
+        height: 630,
+        alt: 'moveeブログ',
+      },
+    ],
+    type: 'website',
+  },
 }
 
 export default function RootLayout({
@@ -15,7 +28,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="ja"> {/* 日本語ページなら "ja" */}
       <body className={inter.className}>{children}</body>
     </html>
   )
