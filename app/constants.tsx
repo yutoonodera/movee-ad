3a28a0dbcb8d65db9c65122a64005faed70e5927
onody <onoderayuto@onody00.local>
2025-04-07 21:59:42 +0900

    リンク設定

---
+++ app/constants.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const TITLE = 'moveeブログ';
+export const DESCRIPTION = '株式会社moveeの公式ブログです。ソフトウェア開発に関する情報を発信しています。';
+export const OPEN_GRAPH_IMAGE = '/images/default.png';
+export const OPEN_GRAPH_IMAGE_WIDTH = '1200';
+export const OPEN_GRAPH_IMAGE_HEIGHT = '630';
+export const MOVEE_USER = 'moveeユーザー';
+export const FINISH_GREETING = '最後までお読みいただきありがとうございました。';
+
+export const CATCH_COPY = (
+    <a href="/" className="text-black-600">
+      Web Software With Movee
+    </a>
+  );
