c4b1388bc8966776def908f85cc1643084b16ee2
onody <onoderayuto@onody00.local>
2025-04-14 21:04:46 +0900

    テキスト修正

--- app/constants.tsx
+++ app/constants.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 export const TITLE = '💻株式会社moveeは、福岡でWebサイト・システム開発を行う会社';
-export const BLOG_TITLE = '🗒️株式会社moveeのブログページ';
+export const BLOG_TITLE = '🗒️株式会社moveeのブログ';
 export const DESCRIPTION = '株式会社moveeは、福岡でWebサイト・システム開発を行う会社です。企業活動に貢献する自社サービスや受託開発・SES事業を展開しています。';
 export const BLOG_DESCRIPTION = '株式会社moveeのブログページです。ソフトウェア開発に関する情報を発信しています。';
 export const OPEN_GRAPH_IMAGE = '/images/default.png';
